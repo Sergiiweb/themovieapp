d5b6fe934605c2555716a9431f29aa7c5146553a
Sergii <sergii.web@gmail.com>
2024-01-31 20:06:36 +0200

    The Movie App For deploy on Github Pages

--- src/router.tsx
+++ src/router.tsx
@@ -10,18 +10,18 @@ const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Navigate to={'themovieapp/movies'}/>
+                element: <Navigate to={'movies'}/>
             },
             {
-                path: 'themovieapp/movies',
+                path: 'movies',
                 element: <MoviesPage/>,
             },
             {
-                path: 'themovieapp/movies/:id',
+                path: 'movies/:id',
                 element: <MovieDetailsPage/>
             },
             {
-                path: 'themovieapp/genres',
+                path: 'genres',
                 element: <GenresPage/>,
             },
             {
